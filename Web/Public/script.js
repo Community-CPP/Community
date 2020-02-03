9904e7fe3403ec7aa91a757e2e666e1732b1ae47
pedro12133 <pedrobeltran12133@gmail.com>
2020-02-02 22:41:04 -0800

    Added a sample database to query.

---
+++ Web/Public/script.js
@@ -0,0 +1,12 @@
+// getComplexes();
+// function getComplexes() {
+//     $.get('/', function(data) {
+//         if(!data) {
+//             console.log('No data recieved.');
+//         }
+//         console.log("Data recieved: ");
+//         for(var i = 0; i < data.length; i++) {
+//             console.log(data[i].name);
+//         }
+//     });
+// }
--- Web/server.js
+++ Web/server.js
@@ -1,10 +1,30 @@
 var express = require('express');
 var app = express();
+var sqlite3 = require('sqlite3');
+var db = new sqlite3.Database('/home/pedro/Desktop/Community_App/Community/Database/complexes.db');
+
+//app.use('/home/pedro/Desktop/Community_App/Community/Web/Public');
 
 //routes
 app.get('/', function(request, response) {
-    response.send('Hello world!');
+    db.all('SELECT * FROM complexes', function(err,rows) {
+        if(err) {
+            console.log('Error: '+err);
+        }
+        else {
+            response.send(rows);
+        }
+    });
 });
+
+app.get('/complexes', function(request, response) {
+    console.log('GET request recieeved at /complexes');
+});
+
+app.post('/complexes', function(request, response) {
+    console.log('POST request recieeved at /complexes');
+});
+
 app.listen(3000,function(){
     console.log("server is running on port 3000")
 });
