fee95a2924ad653c876248b944e26164b5396afb
Eric Ramirez <primerica24@yahoo.com>
2020-03-21 21:10:08 -0700

    option list

--- Mobile/community_app/lib/ScheduleMaintenance.dart
+++ Mobile/community_app/lib/ScheduleMaintenance.dart
@@ -6,6 +6,25 @@ class ScheduleMaintenance extends StatefulWidget{
 }
 
 class _ScheduleMaintenance extends State<ScheduleMaintenance>{
+  int selectedRadio;
+  int selectedRadioTile;
+
+  @override
+  void initState(){
+    super.initState();
+    selectedRadio = 0;
+    selectedRadioTile = 0;
+  }
+  setSelectedRadio(int val){
+    setState(() {
+      selectedRadio = val;
+    });
+  }
+  setSelectedRadioTile(int val){
+    setState(() {
+      selectedRadioTile = val;
+    });
+  }
   @override
   Widget build(BuildContext context){
     return new Scaffold(
@@ -13,6 +32,68 @@ class _ScheduleMaintenance extends State<ScheduleMaintenance>{
         title: new Text('Schedule Maintenance'),
         backgroundColor: Colors.black54,
       ),
+      body: Column(
+
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          RadioListTile(
+            value: 1,
+            groupValue: selectedRadioTile,
+            title: Text("Electricity"),
+            onChanged: (val){
+              print("Electricity selected $val");
+              setSelectedRadioTile(val);
+            },
+          selected: false,
+            activeColor: Colors.black,
+          ),
+          RadioListTile(
+            value: 2,
+            groupValue: selectedRadioTile,
+            title: Text("Plumbing"),
+            onChanged: (val){
+              print("Plumbing Selected $val");
+              setSelectedRadioTile(val);
+            },
+            selected: false,
+            activeColor: Colors.black,
+          ),
+          RadioListTile(
+            value: 3,
+            groupValue: selectedRadioTile,
+            title: Text("Heater/Air Conditioner"),
+            onChanged: (val){
+              print("Heater/Air Conditioner selected $val");
+              setSelectedRadioTile(val);
+            },
+            selected: false,
+            activeColor: Colors.black,
+          ),
+          RadioListTile(
+            value: 4,
+            groupValue: selectedRadioTile,
+            title: Text("Flooring"),
+            onChanged: (val){
+              print("Flooring pressed $val");
+              setSelectedRadioTile(val);
+            },
+            selected: false,
+            activeColor: Colors.black,
+          ),
+          RadioListTile(
+            value: 5,
+            groupValue: selectedRadioTile,
+            title: Text("Other"),
+            onChanged: (val){
+              print("other selected $val");
+              setSelectedRadioTile(val);
+            },
+            selected: false,
+            activeColor: Colors.black,
+          ),
+        ]
+      )
+
     );
   }
 }
