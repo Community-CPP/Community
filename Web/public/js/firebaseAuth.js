1c24e7d3c9f90688d844d6dc3b5def541e47dd3e
pedro12133 <pedrobeltran12133@gmail.com>
2020-04-06 22:32:21 -0700

    refactored authentication for login with email and password

--- Web/public/js/firebaseAuth.js
+++ Web/public/js/firebaseAuth.js
@@ -5,46 +5,74 @@
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-function checkIfLoggedIn() {
-    
-        console.log('not log in page');
-    firebase.auth().onAuthStateChanged(function(user) {
-        if(user) {
-            console.log(user);
-            
-        }
-        else {
-            console.log("signed out");
-            if(!window.location.href.includes('log-in'))
-                window.location = 'log-in';
-        }
-    });  
-}    
-
-window.onload = function () {
-   this.checkIfLoggedIn(); 
+/////////ATHENTICATION//////////
+ 
+// sign out
+async function signOut() {
+    await firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+        window.location.pathname = '/';
+      }).catch(function(error) {
+        // An error happened.
+      });
 }
 
-window.onpaint = checkIfLoggedIn();
-
-function signOut() {
-    firebase.auth().signOut();
-    document.getElementById("profile-pic").setAttribute('src',"");
-    document.getElementById("display-name").setAttribute('style',"visibility: hidden");
-    document.getElementById("google-email").setAttribute('style',"visibility: hidden");
-    checkIfLoggedIn();
+// sign in with email and password
+async function signIn(email, password) {
+    return await firebase.auth().signInWithEmailAndPassword(email, password);
 }
 
-// sign in with pop up using Google Auth
-function signIn() {
-    var provider = new firebase.auth.GoogleAuthProvider();
-    provider.addScope('profile');
-    provider.addScope('email');
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-        window.location = 'dashboard';
+// authenticate new user
+ async function createNewUser(email, password) {
+    return await firebase.auth().createUserWithEmailAndPassword(email, password);
+}
+
+/////////DATABASE//////////
+
+//initialize firestore
+var db = firebase.firestore();
+
+// initialize new user
+async function initializeNewUserData(firstName,lastName,userCategory) {
+    await firebase.auth().onAuthStateChanged(function(user) {
+         if(user) {
+            addUserToDB(user.uid,firstName,lastName,userCategory,user.email);
+        }; 
+    })
+}
+
+// add new user data to db
+async function addUserToDB(uid,firstName,lastName,userCategory,userEmail) {
+    console.log("in add user")
+    await db.collection("users").doc(uid).set({
+        first: firstName,
+        last: lastName,
+        category: userCategory,
+        email: userEmail
+    })
+    .then(function(docRef) {
+        window.location.pathname = 'dashboard';
     })
     .catch(function(error) {
-        console.log(error);
+        // error adding user
     });
-    checkIfLoggedIn();
 }
+
+// on window load, check login state
+window.onload = async function() {
+    this.console.log(window.location.pathname);
+    await firebase.auth().onAuthStateChanged(function(user) {
+        if(user) {
+            if(window.location.pathname === "/login.html") {
+                window.location.pathname = 'dashboard';
+            }
+            if(window.location.pathname === "/dashboard.html")
+                document.getElementsByTagName('body')[0].hidden = false;
+        }
+        else {
+            console.log("Logged out");
+            if(window.location.pathname === "/dashboard.html")
+                window.location.pathname = "login"; 
+        }
+    }); 
+};
