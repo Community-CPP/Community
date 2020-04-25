62b0779a8e6f22b2633cc5e192ab1657e1fbf99a
pedro12133 <pedrobeltran12133@gmail.com>
2020-04-24 21:51:24 -0700

    User class

---
+++ Web/public/js/user.js
@@ -0,0 +1,9 @@
+class User {
+    constructor(UID,first,last,communities,category) {
+      this.uid = UID;
+      this.first = first;
+      this.last = last;
+      this.communities = communities;
+      this.category = category;
+    }
+  }
